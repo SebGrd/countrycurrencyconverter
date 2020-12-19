a3583113c391677d0c3cfaa766f01c9cd75f7965
SebGrd <sebastien.gaudard.1@gmail.com>
2020-12-19 20:53:38 +0100

    feat: country search

--- src/views/CountryList.jsx
+++ src/views/CountryList.jsx
@@ -1,9 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 
 import CountryCard from "../components/CountryCard";
 
+function SearchInput({setFilteredCountryData, countryData}) {
+    let [search, setSearch] = useState('')
+
+    useEffect(() => {
+        if (!search) {
+            setFilteredCountryData(countryData)
+            return null
+        }
+        setFilteredCountryData(countryData?.filter(({name}) =>
+            name.toLowerCase().includes(search.toLocaleLowerCase()))
+        )
+    }, [search, countryData, setFilteredCountryData])
+
+    const handleChange = (event) => {
+        setSearch(event.target.value)
+    }
+
+    return (
+        <input className="country-list__header__input" type="text" onChange={handleChange}/>
+    )
+}
+
 export default function CountryList() {
     let [countryData, setCountryData] = useState([])
+    let [filteredCountryData, setFilteredCountryData] = useState([])
 
     useEffect(() => {
         const fetchData = () => {
@@ -17,15 +40,20 @@ export default function CountryList() {
     return (
         <>
             <div className="container">
-                <h1>Country list</h1>
+                <div className="country-list__header">
+                    <h1 className="country-list__header__title">Country list</h1>
+                    <SearchInput
+                        setFilteredCountryData={setFilteredCountryData}
+                        countryData={countryData}
+                    />
+                </div>
                 <div className="row">
-                    { countryData?.map( ({name, alpha2Code, region, currencies, flag}) =>
+                    {filteredCountryData?.map(({name, alpha2Code, region, currencies, flag}) =>
                         <CountryCard
-                            key={`country-${name}`}
+                            key={`country-${alpha2Code}`}
                             countryData={{name, alpha2Code, region, currencies, flag}}
                         />
-                    )
-                    }
+                    )}
                 </div>
             </div>
         </>
